1f141c01e8955331d1b2090a8c0a48911d2befea
naymayakub <47854378+naymayakub@users.noreply.github.com>
2019-04-06 09:56:17 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+def subtract(a,b):
+ 	f=a-b
+ 	print(f)
+
+def multiply(a,b):
+	g=a*b
+	print(g)
+
+def division(a,b):
+	h=a/b
+	print(h)
+
+def modulus(a,b):
+	i=a%b
+	print(i)
+
+def percentage(a,b):
+	j=a/b*100
+	h=b/a*100
+	print(j,+h)
+
+
+def exponet(a,b):
+	k=a**b
+	print(k)
+
+def wholedivision(a,b):
+	l=a//b
+	print(l)
+
+	
